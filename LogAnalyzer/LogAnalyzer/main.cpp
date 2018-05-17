8a2efb61e7a554208fda60193267908dd4c20213
nappozord <38859529+nappozord@users.noreply.github.com>
2018-05-17 10:36:24 +0200

    Add files via upload

--- LogAnalyzer/LogAnalyzer/main.cpp
+++ LogAnalyzer/LogAnalyzer/main.cpp
@@ -20,7 +20,8 @@ using namespace std;
 // 
 
 void analyzer(char filename[], string temp){
-    User *p[100];
+    //User *p[100];
+    vector <unique_ptr<User> > people;
 
     string s;
     sprintf(filename, "rec_%s.txt", temp.c_str());
@@ -40,7 +41,6 @@ void analyzer(char filename[], string temp){
         string i;
         string word;
         stringstream stream(s);
-        
         int count=0;
         while(getline(stream, word, ',')){
             // CICLO SU OGNI TOKEN es. "-94" "12841338" "da:a1:19:93:81:96"
@@ -64,15 +64,17 @@ void analyzer(char filename[], string temp){
                     {
                         auto index = distance(mac.begin(), it);
                         cout << "MAC already in List\nAppending Probe Request to user Nr. "<< index<<"\n";
-                        p[index]->append_prob(rssi.back(), ts.back());
-                        cout << "Numero di Probe Request inviate:"<< p[index]->get_n_prob()<<"\n\n"<<endl;
+                        people.at(index)->append_prob(rssi.back(), ts.back());
+                        cout << "Numero di Probe Request inviate:"<< people.at(index)->get_n_prob()<<"\n\n"<<endl;
 
                     }
                     else{
                         cout << "Creating New User nr."<<(int) mac.size()<<" \n"<<endl;
                         mac.push_back(word);
                         // Istanzio nuova persona
-                        p[(int) mac.size()-1]=new User (rssi.back(), ts.back(), mac.back());
+                        people.push_back(unique_ptr<User>(new User(rssi.back(), ts.back(), mac.back())));
+
+                        //p[(int) mac.size()-1]=new User (rssi.back(), ts.back(), mac.back());
     
                     }
                     count++;
@@ -88,7 +90,7 @@ void analyzer(char filename[], string temp){
     // Fine While riga
     for (int per=0; per<mac.size(); per++) {
 
-        cout<<"Dispositivo nr: " <<per <<": "<< p[per]->get_mac()<<" ha inviato "<<p[per]->get_n_prob()<<" Prob Request"<<endl;
+        cout<<"Dispositivo nr: " <<per <<": "<< people.at(per)->get_mac()<<" ha inviato "<<people.at(per)->get_n_prob()<<" Prob Request"<<endl;
         
     }
     
